d29db8414e2b6d23a7ef6dd20604e6f5846f7e4c
Tayfun Gürlevik <tgurlevik@gmail.com>
2018-11-02 13:40:46 +0300

    first commit

---
+++ Chicken-Man/Assets/Scripts/InputComponent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComponent : MonoBehaviour
+{
+    public float Horizontal;
+    public float Vertical;
+}
---
+++ Chicken-Man/Assets/Scripts/InputSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class InputSystem : ComponentSystem
+{
+    private struct Data
+    {
+        public readonly int  Length;
+        public ComponentArray<InputComponent> InputComponents;
+    }
+    [Inject] private Data data;
+    protected override void OnUpdate()
+    {
+#if UNITY_EDITOR
+        for (int i = 0; i < data.Length; i++)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            data.InputComponents[i].Horizontal = horizontal;
+            data.InputComponents[i].Vertical = vertical;
+        }
+#else
+        
+#endif
+
+    }
+}
